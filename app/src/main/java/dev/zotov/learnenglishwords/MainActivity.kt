0965c9ed17055f45f1e34b9ce2fe58d9d4ccbc7e
zotovy <the1ime@yandex.ru>
2024-05-08 16:50:43 +0300

    fix: remove unused icon

--- app/src/main/java/dev/zotov/learnenglishwords/MainActivity.kt
+++ app/src/main/java/dev/zotov/learnenglishwords/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.zotov.learnenglishwords
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,5 +20,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
